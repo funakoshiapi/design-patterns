b96156a68a250b37db2e2ef1a1e875e7fbb9e6c9
Funakoshi Silva <55293531+funakoshiapi@users.noreply.github.com>
2021-09-27 09:58:10 -0400

    Add files via upload

---
+++ Strategy 1/Program.cs
@@ -0,0 +1,100 @@
+﻿using Strategy_Pattern_First_Look.Business.Models;
+using Strategy_Pattern_First_Look.Business.Strategies.Invoice;
+using Strategy_Pattern_First_Look.Business.Strategies.SalesTax;
+using Strategy_Pattern_First_Look.Business.Strategies.Shipping;
+using System;
+
+namespace Strategy_Pattern_First_Look
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select an origin country: ");
+            var origin = Console.ReadLine().Trim();
+
+            Console.WriteLine("Please select a destination country: ");
+            var destination = Console.ReadLine().Trim();
+
+            Console.WriteLine("Choose one of the following shipping providers.");
+            Console.WriteLine("1. DHL");
+            Console.WriteLine("2. UPS");
+            Console.WriteLine("3. USPS");
+            Console.WriteLine("Select shipping provider: ");
+            var provider = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Console.WriteLine("Choose one of the following invoice delivery options.");
+            Console.WriteLine("1. File");
+            Console.WriteLine("2. E-mail");
+            Console.WriteLine("3. Mail");
+            Console.WriteLine("Select invoice delivery options: ");
+            var invoiceOption = Convert.ToInt32(Console.ReadLine().Trim());
+
+
+
+            var order = new Order
+            {
+                ShippingDetails = new ShippingDetails
+                {
+                    OriginCountry = origin,
+                    DestinationCountry = destination,
+
+                    /// Todo: make distintion between local(inside usa) and international shipping 
+                    DestinationState = destination
+                },
+                SalesTaxStrategy = GetTaxStrategyFor(origin),
+                InvoiceStrategy = GetInvoiceStrategyFor(invoiceOption),
+                ShippingStrategy = GetShippingStrategyFor(provider)
+                
+            };
+
+            order.SelectedPayments.Add(new Payment { PaymentProvider = PaymentProvider.Invoice });
+            
+            order.LineItems.Add(new Item("CSHARP_SMORGASBORD", "C# Smorgasbord", 100m, ItemType.Literature), 1);
+            order.LineItems.Add(new Item("CONSULTING", "Building a website", 100m, ItemType.Service), 1);
+
+            Console.WriteLine(order.GetTax());
+            order.FinalizeOrder();
+        }
+
+
+        private static IInvoiceStrategy GetInvoiceStrategyFor(int option)
+        {
+            switch(option)
+            {
+                case 1: return new FileInvoiceStrategy();
+                case 2: return new EmailInvoiceStrategy();
+                case 3: return new PrintOnDemandInvoiceStrategy();
+                default: throw new Exception("Unsupported invoice delivery option");
+            }
+        }
+
+        private static IShippingStrategy GetShippingStrategyFor(int provider)
+        {
+            switch(provider)
+            {
+                case 1: return new DHLShippingStrategy();
+                case 2: return new UpsShippingStrategy();
+                case 3: return new USPSShippingStrategy();
+                default: throw new Exception("Unsupported shipping method");
+            }
+        }
+
+        private static ISalesTaxStrategy GetTaxStrategyFor(string origin)
+        {
+            if(origin.ToLowerInvariant() == "sweden")
+            {
+                return new SwedenSalesTaxStrategy();
+            }
+            else if (origin.ToLowerInvariant() == "usa")
+            {
+                return new USSalesTaxStrategy();
+            }
+            else
+            {
+                throw new Exception("Unsupported shipping region");
+            }
+        }
+
+    }
+}
