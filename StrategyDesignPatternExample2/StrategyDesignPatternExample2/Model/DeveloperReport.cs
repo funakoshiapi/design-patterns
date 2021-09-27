fe841ccff77c067419b88249ae0335a3b7e5e2de
Funakoshi Silva <55293531+funakoshiapi@users.noreply.github.com>
2021-09-27 09:56:25 -0400

    Add files via upload

---
+++ StrategyDesignPatternExample2/StrategyDesignPatternExample2/Model/DeveloperReport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyDesignPatternExample2.Model
+{
+    public enum DeveloperLevel
+    {
+        Senior,
+        Junior
+    }
+
+    public class DeveloperReport
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DeveloperLevel Level { get; set; }
+        public int WorkingHours { get; set; }
+        public double HourlyRate { get; set; }
+
+        public double CalculateTotalSalary() => WorkingHours * HourlyRate;
+    }
+}
